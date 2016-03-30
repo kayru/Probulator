a54702a1905d8c5e124e3c2e6314b3a29f6f4a14
Yuriy O'Donnell <yuriyo@gmail.com>
2016-03-30 23:30:22 +0200

    Added the quick-n-dirty Markdown report generator
    Commented out by default

--- Source/ProbulatorCLI/Main.cpp
+++ Source/ProbulatorCLI/Main.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <sstream>
 #include <string.h>
+#include <regex>
 
 #ifdef _MSC_VER
 #define strcasecmp _stricmp
@@ -110,6 +111,136 @@ void generateReportHtml(const ExperimentList& experiments, const char* filename)
 	f.close();
 }
 
+#if 0
+void generateReportMarkdown(const ExperimentList& experiments, const char* envmapFilename, const char* filename)
+{
+	std::string envmapName = envmapFilename;
+	{
+		size_t pos = envmapName.find_last_of("/\\");
+		if (std::string::npos != pos)
+		{
+			envmapName.erase(0, pos + 1);
+		}
+
+		pos = envmapName.find_last_of(".");
+		if (std::string::npos != pos)
+		{
+			envmapName = envmapName.substr(0, pos);
+		}
+
+		envmapName[0] = toupper(envmapName[0]);
+	}
+
+	Experiment* referenceMode = nullptr;
+	for (const auto& it : experiments)
+	{
+		if (it->m_useAsReference && it->m_enabled)
+		{
+			referenceMode = it.get();
+			break;
+		}
+	}
+
+	std::ofstream f;
+	f.open(filename);
+
+	f << "#### " << envmapName << " ####" << std::endl << std::endl;
+
+	f << "| Radiance | Irradiance ";
+	if (referenceMode)
+	{
+		f << " | Irradiance Error (sMAPE)";
+	}
+	f << " | Mode" << std::endl;
+
+	f << "| --- | --- ";
+	if (referenceMode)
+	{
+		f << " | ---";
+	}
+	f << " | ---" << std::endl;
+
+	std::string baseUrl = "https://github.com/kayru/Probulator/raw/master/Reports/" + envmapName + "/";
+	std::ostringstream imageUrls;
+
+	for (const auto& it : experiments)
+	{
+		if (!it->m_enabled)
+			continue;
+
+		std::ostringstream radianceFilename;
+		radianceFilename << "radiance" << it->m_suffix << ".png";
+		it->m_radianceImage.writePng(radianceFilename.str().c_str());
+
+		std::ostringstream irradianceFilename;
+		irradianceFilename << "irradiance" << it->m_suffix << ".png";
+		it->m_irradianceImage.writePng(irradianceFilename.str().c_str());
+
+		std::ostringstream radianceText;
+		if (referenceMode && referenceMode != it.get())
+		{
+			vec4 mse = imageMeanSquareError(referenceMode->m_radianceImage, it->m_radianceImage);
+			float mseScalar = dot(vec3(1.0f / 3.0f), (vec3)mse);
+			radianceText << "MSE: " << mseScalar << " ";
+			radianceText << "RMS: " << sqrtf(mseScalar);
+		}
+
+		std::ostringstream irradianceText;
+		if (referenceMode && referenceMode != it.get())
+		{
+			vec4 mse = imageMeanSquareError(referenceMode->m_irradianceImage, it->m_irradianceImage);
+			float mseScalar = dot(vec3(1.0f / 3.0f), (vec3)mse);
+			irradianceText << "MSE: " << mseScalar << " ";
+			irradianceText << "RMS: " << sqrtf(mseScalar);
+		}
+
+		f << "| ![Radiance] [" << envmapName << "-" << radianceFilename.str() << "]";
+		imageUrls << "[" << envmapName << "-" << radianceFilename.str() << "]: " << baseUrl << radianceFilename.str() << std::endl;
+
+		f << "| ![Irradiance] [" << envmapName << "-" << irradianceFilename.str() << "]";
+		imageUrls << "[" << envmapName << "-" << irradianceFilename.str() << "]: " << baseUrl << irradianceFilename.str() << std::endl;
+
+		if (referenceMode)
+		{
+			if (referenceMode != it.get())
+			{
+				std::ostringstream irradianceErrorFilename;
+				irradianceErrorFilename << "irradianceError" << it->m_suffix << ".png";
+				it->m_radianceImage.writePng(irradianceErrorFilename.str().c_str());
+
+				Image errorImage = imageSymmetricAbsolutePercentageError(referenceMode->m_irradianceImage, it->m_irradianceImage);
+				for (vec4& pixel : errorImage)
+				{
+					pixel.w = 1.0f;
+				}
+				errorImage.writePng(irradianceErrorFilename.str().c_str());
+
+				f << "| ![IrradianceError] [" << envmapName << "-" << irradianceErrorFilename.str() << "]";
+				imageUrls << "[" << envmapName << "-" << irradianceErrorFilename.str() << "]: " << baseUrl << irradianceErrorFilename.str() << std::endl;
+			}
+			else
+			{
+				f << " | **N/A**";
+			}
+		}
+
+		f << " | " << std::regex_replace(it->m_name, std::regex("\\["), "<br>[");
+		if (referenceMode == it.get())
+		{
+			f << "<br>**Reference**";
+		}
+
+		f << std::endl;
+	}
+
+	f << std::endl;
+	f << imageUrls.str();
+	f << std::endl;
+
+	f.close();
+}
+#endif
+
 static void enableExperimentsBySuffix(ExperimentList& list, u32 suffixCount, char** suffixes)
 {
 	for (const auto& e : list)
@@ -169,6 +300,7 @@ int main(int argc, char** argv)
 	}
 
 	generateReportHtml(experiments, "report.html");
+	// generateReportMarkdown(experiments, inputFilename, "report.md");
 
 	return 0;
 }
