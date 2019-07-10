a2481b53ee323ee231140b7e1e669c9392ac0c63
Yuriy O'Donnell <yuriyo@gmail.com>
2019-07-10 11:21:49 +0200

    Added a script to run clang-format on all source files

---
+++ Scripts/run-clang-format.sh
@@ -0,0 +1,3 @@
+clang-format -i ../Source/Probulator/*.h ../Source/Probulator/*.cpp
+clang-format -i ../Source/ProbulatorCLI/*.cpp
+clang-format -i ../Source/ProbulatorGUI/*.h ../Source/ProbulatorGUI/*.cpp
